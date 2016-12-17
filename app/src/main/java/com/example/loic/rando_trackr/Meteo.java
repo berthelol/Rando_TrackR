aa6b67604762b5894cb7973dc9eaa6a37cd9b777
mathieuchebassier <mathieu.chebassier@edu.ece.fr>
2016-12-17 20:08:35 +0100

    A gérer : Trop long au chargement Méteo avec le point d'arrivée ajoutée

--- app/src/main/java/com/example/loic/rando_trackr/Meteo.java
+++ app/src/main/java/com/example/loic/rando_trackr/Meteo.java
@@ -2,10 +2,12 @@ package com.example.loic.rando_trackr;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.loic.rando_trackr.MeteoObject.ImageConnection_Handler;
@@ -33,6 +36,9 @@ import java.util.Calendar;
 import java.util.concurrent.ExecutionException;
 
 import static android.support.v7.widget.AppCompatDrawableManager.get;
+import static com.example.loic.rando_trackr.R.id.imageView;
+import static com.example.loic.rando_trackr.R.id.layoutA;
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 /**
@@ -43,14 +49,17 @@ public class Meteo extends Fragment {
 
     private EditText cityName;
     private TextView txt_temp_h1, txt_temp_h2, txt_temp_h3, txt_temp_h4,
-                    txt_temp_j1, txt_temp_j2, txt_temp_j3, txt_city,
-                    txt_h1, txt_h2, txt_h3, txt_h4, txt_j1, txt_j2, txt_j3;
+                    txt_temp_j1, txt_temp_j2, txt_temp_j3, txt_temp_j1A, txt_temp_j2A, txt_temp_j3A, txt_city, txt_cityA,
+                    txt_h1, txt_h2, txt_h3, txt_h4, txt_j1, txt_j2, txt_j3, txt_j1A, txt_j2A, txt_j3A;
 
     private ImageView img_h1, img_h2, img_h3, img_h4,
-                    img_j1, img_j2, img_j3;
+                    img_j1, img_j2, img_j3, img_j1A, img_j2A, img_j3A;
     private ImageButton btn;
 
-    private Weather weatherh1, weatherh2, weatherh3, weatherh4, weatherj1, weatherj2, weatherj3;
+    private float latD, latA, lonD, lonA;
+    private Weather weatherh1, weatherh2, weatherh3, weatherh4, weatherj1, weatherj2, weatherj3, weatherj1A, weatherj2A, weatherj3A;
+    private LinearLayout layoutA, layoutIF;
+
 
     @Nullable
     @Override
@@ -66,6 +75,18 @@ public class Meteo extends Fragment {
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Meteo");
 
+
+        // We initialize to 0 each value to know that there is no arrival point
+        latA = 0;
+        lonA = 0;
+
+        ///////////        LOIC        --------------------------------->
+        // Then we get the current position and arrival position from the app
+        latD = Float.parseFloat(""+48.85);
+        lonD = Float.parseFloat(""+2.35);
+        // + datas d'arrivée ;)
+
+
         // TextView where we print the temperatures
         // with ...h for nexts hours
         txt_temp_h1 = (TextView) getView().findViewById(R.id.txt_temp_h1);
@@ -78,6 +99,10 @@ public class Meteo extends Fragment {
         txt_temp_j2 = (TextView) getView().findViewById(R.id.txt_temp_j2);
         txt_temp_j3 = (TextView) getView().findViewById(R.id.txt_temp_j3);
 
+        txt_temp_j1A = (TextView) getView().findViewById(R.id.txt_temp_j1A);
+        txt_temp_j2A = (TextView) getView().findViewById(R.id.txt_temp_j2A);
+        txt_temp_j3A = (TextView) getView().findViewById(R.id.txt_temp_j3A);
+
         txt_h1 = (TextView) getView().findViewById(R.id.txt_h1);
         txt_h2 = (TextView) getView().findViewById(R.id.txt_h2);
         txt_h3 = (TextView) getView().findViewById(R.id.txt_h3);
@@ -87,7 +112,12 @@ public class Meteo extends Fragment {
         txt_j2 = (TextView) getView().findViewById(R.id.txt_j2);
         txt_j3 = (TextView) getView().findViewById(R.id.txt_j3);
 
+        txt_j1A = (TextView) getView().findViewById(R.id.txt_j1A);
+        txt_j2A = (TextView) getView().findViewById(R.id.txt_J2A);
+        txt_j3A = (TextView) getView().findViewById(R.id.txt_J3A);
+
         txt_city = (TextView) getView().findViewById(R.id.txt_city);
+        txt_cityA = (TextView) getView().findViewById(R.id.txt_cityA);
 
         // ImageView for the meteo
 
@@ -100,8 +130,34 @@ public class Meteo extends Fragment {
         img_j2 = (ImageView) getView().findViewById(R.id.image_j2);
         img_j3 = (ImageView) getView().findViewById(R.id.image_j3);
 
+        img_j1A = (ImageView) getView().findViewById(R.id.image_j1A);
+        img_j2A = (ImageView) getView().findViewById(R.id.image_j2A);
+        img_j3A = (ImageView) getView().findViewById(R.id.image_j3A);
+
+
+        layoutA = (LinearLayout) getView().findViewById(R.id.layoutA);
+        layoutIF = (LinearLayout) getView().findViewById(R.id.layoutIF);
+
         // We initialize meteo datas
         weatherUpdate();
+        if(lonA != 0 || latA != 0){
+            // Get the meteo of arrival point
+            weatherUpdateArrival();
+
+            // We print the meteo of arrival point
+            layoutA.setVisibility(View.VISIBLE);
+
+            // We desable the message
+            layoutIF.setVisibility(View.INVISIBLE);
+
+        }
+        else
+        {
+            // Desable view of the meteo of arrival point
+            layoutA.setVisibility(View.INVISIBLE);
+            // Active the information message
+            layoutIF.setVisibility(View.VISIBLE);
+        }
     }
 
     // http://www.survivingwithandroid.com/2013/05/build-weather-app-json-http-android.html
@@ -269,11 +325,13 @@ public class Meteo extends Fragment {
         final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/?";
         //Weather web site API KEY
         final String API_KEY = getResources().getString(R.string.weather_API_KEY);
+
+        DecimalFormat df = new DecimalFormat("###.#");
+
         // Get the new weather
         Connection_Handler ask_for_weather = new Connection_Handler();
         //Execute the request
-        ask_for_weather.execute(BASE_URL+"lat=48.85&lon=2.35"+"&APPID="+API_KEY);
-
+        ask_for_weather.execute(BASE_URL+"lat="+ latD +"&lon="+ lonD +"&APPID="+API_KEY);
 
         String response= null;
         try {
@@ -288,24 +346,46 @@ public class Meteo extends Fragment {
             //Update the view
             // Dates sous la forme : 2016-12-18 06:00:00
             // Extract hour from the date string
-            if(weatherh1 != null) txt_h1.setText(weatherh1.getDate().substring(11, 16));
-            if(weatherh2 != null) txt_h2.setText(weatherh2.getDate().substring(11, 16));
-            if(weatherh3 != null) txt_h3.setText(weatherh3.getDate().substring(11, 16));
-            if(weatherh4 != null) txt_h4.setText(weatherh4.getDate().substring(11, 16));
+            // And the temperature
+            if(weatherh1 != null)
+            {
+                txt_h1.setText(weatherh1.getDate().substring(11, 16));
+                txt_temp_h1.setText(""+(df.format(weatherh1.getTemp()))+" °C");
+            }
+            if(weatherh2 != null)
+            {
+                txt_h2.setText(weatherh2.getDate().substring(11, 16));
+                txt_temp_h2.setText(""+(df.format(weatherh2.getTemp()))+" °C");
+            }
+            if(weatherh3 != null)
+            {
+                txt_h3.setText(weatherh3.getDate().substring(11, 16));
+                txt_temp_h3.setText(""+(df.format(weatherh3.getTemp()))+" °C");
+            }
+            if(weatherh4 != null)
+            {
+                txt_h4.setText(weatherh4.getDate().substring(11, 16));
+                txt_temp_h4.setText(""+(df.format(weatherh4.getTemp()))+" °C");
+            }
             // Extract day from the date string
-            if(weatherj1 != null) txt_j1.setText(weatherj1.getDate().substring(5, 10));
-            if(weatherj2 != null) txt_j2.setText(weatherj2.getDate().substring(5, 10));
-            if(weatherj3 != null) txt_j3.setText(weatherj3.getDate().substring(5, 10));
+            if(weatherj1 != null) {
+                txt_j1.setText(weatherj1.getDate().substring(5, 10));
+                txt_temp_j1.setText(""+(df.format(weatherj1.getTemp()))+" °C");
+            }
+            if(weatherj2 != null)
+            {
+                txt_j2.setText(weatherj2.getDate().substring(5, 10));
+                txt_temp_j2.setText(""+(df.format(weatherj2.getTemp()))+" °C");
+            }
+            if(weatherj3 != null)
+            {
+                txt_j3.setText(weatherj3.getDate().substring(5, 10));
+                txt_temp_j3.setText(""+(df.format(weatherj3.getTemp()))+" °C");
+            }
 
-            // Temperatures
-            DecimalFormat df = new DecimalFormat("###.#");
-            if(weatherh1 != null) txt_temp_h1.setText(""+(df.format(weatherh1.getTemp()))+" °C");
-            if(weatherh2 != null) txt_temp_h2.setText(""+(df.format(weatherh2.getTemp()))+" °C");
-            if(weatherh3 != null) txt_temp_h3.setText(""+(df.format(weatherh3.getTemp()))+" °C");
-            if(weatherh4 != null) txt_temp_h4.setText(""+(df.format(weatherh4.getTemp()))+" °C");
-            if(weatherj1 != null) txt_temp_j1.setText(""+(df.format(weatherj1.getTemp()))+" °C");
-            if(weatherj2 != null) txt_temp_j2.setText(""+(df.format(weatherj2.getTemp()))+" °C");
-            if(weatherj3 != null) txt_temp_j3.setText(""+(df.format(weatherj3.getTemp()))+" °C");
+            Log.i("Meteo info J1", df.format(weatherj1.getTemp()));
+            Log.i("Meteo info J2", df.format(weatherj2.getTemp()));
+            Log.i("Meteo info J3", df.format(weatherj3.getTemp()));
 
             txt_city.setText(weatherh1.getCity());
         } catch (InterruptedException e) {
@@ -317,6 +397,51 @@ public class Meteo extends Fragment {
         }
 
         getImages();
+    }
+
+    private void weatherUpdateArrival(){
+        //Weather web site API
+        final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/?";
+        //Weather web site API KEY
+        final String API_KEY = getResources().getString(R.string.weather_API_KEY);
+        // Get the new weather
+        Connection_Handler ask_for_weather = new Connection_Handler();
+        //Execute the request
+        ask_for_weather.execute(BASE_URL+"lat="+ latA +"&lon="+ lonA +"&APPID="+API_KEY);
+
+
+        String response= null;
+        try {
+            //Wait for the data to be uploaded (asynchrone)
+            response = ask_for_weather.get();
+
+            Log.i("Weather Content",response);
+            //Extract weather using the response of the API
+            // Initializing all the weathers objects
+            extractWeatherFromResponseA(new JSONObject(response));
+
+            //Update the view
+            // Extract day from the date string
+            if(weatherj1A != null) txt_j1A.setText(weatherj1A.getDate().substring(5, 10));
+            if(weatherj2A != null) txt_j2A.setText(weatherj2A.getDate().substring(5, 10));
+            if(weatherj3A != null) txt_j3A.setText(weatherj3A.getDate().substring(5, 10));
+
+            // Temperatures
+            DecimalFormat df = new DecimalFormat("###.#");
+            if(weatherj1A != null) txt_temp_j1A.setText(""+(df.format(weatherj1A.getTemp()))+" °C");
+            if(weatherj2A != null) txt_temp_j2A.setText(""+(df.format(weatherj2A.getTemp()))+" °C");
+            if(weatherj3A != null) txt_temp_j3A.setText(""+(df.format(weatherj3A.getTemp()))+" °C");
+
+            if(weatherj1A != null) txt_cityA.setText(weatherj1A.getCity());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        getImagesA();
     }
 
     public byte[] imageDownload(String s)
@@ -350,7 +475,7 @@ public class Meteo extends Fragment {
         return null;
     }
 
-    public void getImages(){
+    public void getImagesFromUrl(){
         ImageConnection_Handler ch1 = new ImageConnection_Handler();
         ImageConnection_Handler ch2 = new ImageConnection_Handler();
         ImageConnection_Handler ch3 = new ImageConnection_Handler();
@@ -517,6 +642,98 @@ public class Meteo extends Fragment {
         */
     }
 
+
+    public void getImagesA() {
+        String fnm;
+        int imgId;
+        if (weatherj1A != null) {
+            fnm = "icon" + weatherj1A.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME + ":drawable/" + fnm, null, null);
+
+            img_j1A.setImageBitmap(BitmapFactory.decodeResource(getResources(), imgId));
+
+        }
+        if (weatherj2A != null) {
+            fnm = "icon" + weatherj2A.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME + ":drawable/" + fnm, null, null);
+
+            img_j2A.setImageBitmap(BitmapFactory.decodeResource(getResources(), imgId));
+
+        }
+        if (weatherj3A != null) {
+            fnm = "icon" + weatherj3A.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME + ":drawable/" + fnm, null, null);
+
+            img_j3A.setImageBitmap(BitmapFactory.decodeResource(getResources(), imgId));
+
+        }
+    }
+
+    public void getImages(){
+        String fnm;
+        int imgId;
+
+        if (weatherh1 != null) {
+            fnm = "icon" + weatherh1.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_h1.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+        }
+        if (weatherh2 != null) {
+            fnm = "icon" + weatherh2.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_h2.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+        if (weatherh3 != null) {
+            fnm = "icon" + weatherh3.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_h3.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+        if (weatherh4 != null) {
+            fnm = "icon" + weatherh4.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_h4.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+        if (weatherj1 != null) {
+            fnm = "icon" + weatherj1.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_j1.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+        if (weatherj2 != null) {
+            fnm = "icon" + weatherj2.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_j2.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+        if (weatherj3 != null) {
+            fnm = "icon" + weatherj3.getIcon(); //  this is image file name
+            String PACKAGE_NAME = getApplicationContext().getPackageName();
+            imgId = getResources().getIdentifier(PACKAGE_NAME+":drawable/"+fnm , null, null);
+
+            img_j3.setImageBitmap(BitmapFactory.decodeResource(getResources(),imgId));
+
+        }
+
+    }
+
     private void extractWeatherFromResponse(JSONObject jObj) {
 
         String datej1 = getDate("j1");
@@ -666,4 +883,76 @@ public class Meteo extends Fragment {
         }
 
     }
+
+    private void extractWeatherFromResponseA(JSONObject jObj) {
+
+        String datej1 = getDate("j1");
+        String datej2 = getDate("j2");
+        String datej3 = getDate("j3");
+
+        try {
+            // We extract the location (first data in JSON response)
+            JSONObject cityObj = getObject("city", jObj);
+            JSONObject coordObj = getObject("coord", cityObj);
+            float lat = getFloat("lat", coordObj);
+            float lon = getFloat("lon", coordObj);
+            String city = getString("name", cityObj);
+            String date = null;
+
+            /*
+            String response = task.get();
+            JSONArray json = new JSONArray(response);
+            Log.i("Length",String.valueOf(json.length()));
+            for(int i=0;i<json.length();i++)
+            {
+                Log.i("data",String.valueOf(json.getJSONObject(i).getInt("mesure")));
+                values.add(new Entry(i,json.getJSONObject(i).getInt("mesure")));
+            }
+             */
+
+            // Go the get the date from the list to compare it with the one we are looking for
+            // Save the date code for each to be able to send it to the function that will create the weather
+            // or erase
+            int n = 0;
+            int close = 0;
+            JSONObject childFromListObj, mainObj;
+            JSONArray listObj = jObj.optJSONArray("list");
+            //for(int i = 0; i<listObj.length(); i++)
+            while(close != 8)
+            {
+                // Go through the list until we find the date we want
+                childFromListObj = listObj.getJSONObject(n);
+                date = childFromListObj.getString("dt_txt");
+
+                Log.i("Date extraite", date);
+
+                // check the dates
+                if (date.equals(datej1))
+                {
+                    this.weatherj1A = createWeather(childFromListObj, lat, lon, city);
+
+                    close ++;
+                }
+                if (date.equals(datej2))
+                {
+                    this.weatherj2A = createWeather(childFromListObj, lat, lon, city);
+
+                    close ++;
+                }
+                if (date.equals(datej3))
+                {
+                    this.weatherj3A = createWeather(childFromListObj, lat, lon, city);
+
+                    close ++;
+                }
+
+                n++;
+
+                Log.i("Number of meteos found", ""+close);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
